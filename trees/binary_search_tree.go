7ee68b633abbc4d07361377b89173332b976a0d3
0x0FACED <https.genius@gmail.com>
2024-07-22 16:56:12 +0300

    added: traversals, delete, BST -> bst

--- trees/binary_search_tree.go
+++ trees/binary_search_tree.go
@@ -4,19 +4,11 @@ import (
 	"fmt"
 
 	gocollections "github.com/0x0FACED/go-collections"
+	"github.com/0x0FACED/go-collections/queue"
 )
 
-// If a == b -> return 0
-//
-// If a > b -> return 1
-//
-// If a < b -> return -1
-//
-// val `a` is the new item, val `b` is the tree's item
-type Comparator[T comparable] func(a, b T) int
-
-// BST - Binary Search Tree
-type BST[T comparable] struct {
+// bst - Binary Search Tree
+type bst[T comparable] struct {
 	root *node[T]
 
 	// comparator is the main func to compare two values of type T
@@ -25,32 +17,102 @@ type BST[T comparable] struct {
 	compare Comparator[T]
 }
 
-func NewBST[T comparable](compare Comparator[T]) *BST[T] {
-	return &BST[T]{compare: compare}
+func NewBST[T comparable](compare Comparator[T]) *bst[T] {
+	return &bst[T]{compare: compare}
 }
 
-func (bst *BST[T]) Insert(item T) {
+func (bst *bst[T]) Insert(item T) {
 	bst.root = bst.insertHelper(bst.root, item)
 }
 
-func (bst *BST[T]) Delete(item T) error {
-	panic("not implemented") // TODO: Implement
+func (bst *bst[T]) Delete(item T) error {
+	var err error
+	bst.root, err = bst.deleteHelper(bst.root, item)
+	return err
 }
 
-func (bst *BST[T]) Search(item T) (*T, error) {
+func (bst *bst[T]) Search(item T) (*T, error) {
 	return bst.searchHelper(bst.root, item)
 }
 
-func (bst *BST[T]) InOrder() []T {
-	panic("not implemented") // TODO: Implement
+func (bst *bst[T]) PreOrder() []T {
+	var items []T
+	bst.preOrderHelper(bst.root, &items)
+	return items
 }
 
-func (bst *BST[T]) insertHelper(curr *node[T], item T) *node[T] {
+func (bst *bst[T]) InOrder() []T {
+	var items []T
+	bst.inOrderHelper(bst.root, &items)
+	return items
+}
+
+func (bst *bst[T]) PostOrder() []T {
+	var items []T
+	bst.postOrderHelper(bst.root, &items)
+	return items
+}
+
+func (bst *bst[T]) LevelOrder() []T {
+	return bst.levelOrderHelper()
+}
+
+func (bst *bst[T]) preOrderHelper(curr *node[T], items *[]T) {
+	if curr == nil {
+		return
+	}
+	*items = append(*items, curr.val)
+	bst.inOrderHelper(curr.left, items)
+	bst.inOrderHelper(curr.right, items)
+}
+
+func (bst *bst[T]) inOrderHelper(curr *node[T], items *[]T) {
+	if curr == nil {
+		return
+	}
+
+	bst.inOrderHelper(curr.left, items)
+	*items = append(*items, curr.val)
+	bst.inOrderHelper(curr.right, items)
+}
+
+func (bst *bst[T]) postOrderHelper(curr *node[T], items *[]T) {
+	if curr == nil {
+		return
+	}
+
+	bst.inOrderHelper(curr.left, items)
+	bst.inOrderHelper(curr.right, items)
+	*items = append(*items, curr.val)
+}
+
+func (bst *bst[T]) levelOrderHelper() []T {
+	q := queue.NewDynamicListQueue[node[T]]()
+	items := make([]T, 0)
+	q.Enqueue(*bst.root)
+	for !q.IsEmpty() {
+		child, err := q.Dequeue()
+		if err != nil {
+			return nil
+		}
+		items = append(items, child.val)
+		if child.left != nil {
+			q.Enqueue(*child.left)
+		}
+		if child.right != nil {
+			q.Enqueue(*child.right)
+		}
+	}
+
+	return items
+}
+
+func (bst *bst[T]) insertHelper(curr *node[T], item T) *node[T] {
 	if curr == nil {
 		return &node[T]{val: item}
 	}
-	// if new item < curr.val (tree's val) than we go to left subtree
-	// else -> our new value > curr.val -> go to right
+	// if new item < curr.val compare returns -1 (tree's val) than we go to left subtree
+	// else -> our new value >= curr.val -> go to right
 	if bst.compare(item, curr.val) == -1 {
 		curr.left = bst.insertHelper(curr.left, item)
 	} else {
@@ -59,7 +121,55 @@ func (bst *BST[T]) insertHelper(curr *node[T], item T) *node[T] {
 	return curr
 }
 
-func (bst *BST[T]) searchHelper(curr *node[T], item T) (*T, error) {
+func (bst *bst[T]) deleteHelper(curr *node[T], item T) (*node[T], error) {
+	if curr == nil {
+		return curr, fmt.Errorf(gocollections.ErrNotFound)
+	}
+
+	res := bst.compare(item, curr.val)
+	var err error
+	// if res == 0 -> item found in tree
+	if res == 0 {
+		if curr.left == nil && curr.right == nil {
+			return nil, nil
+		} else if curr.left == nil {
+			return curr.right, nil
+		} else if curr.right == nil {
+			return curr.left, nil
+		}
+
+		// lets find min element of right subtree
+		rightMin := bst.findMin(curr.right)
+		curr.val = rightMin.val
+		curr.right, err = bst.deleteHelper(curr.right, rightMin.val)
+		if err != nil {
+			return curr, err
+		}
+	} else if res == -1 { // if item < curr.val -> go left
+		curr.left, err = bst.deleteHelper(curr.left, item)
+		// if err != nil -> item not found, return err (gocollections.NotFound)
+		if err != nil {
+			return curr, err
+		}
+	} else { // else item > curr.val -> go right
+		curr.right, err = bst.deleteHelper(curr.right, item)
+		// if err != nil -> item not found, return err (gocollections.NotFound)
+		if err != nil {
+			return curr, err
+		}
+	}
+
+	return curr, nil
+}
+
+func (bst *bst[T]) findMin(n *node[T]) *node[T] {
+	for n.left != nil {
+		n = n.left
+	}
+	return n
+}
+
+func (bst *bst[T]) searchHelper(curr *node[T], item T) (*T, error) {
 	if curr == nil {
 		return nil, fmt.Errorf(gocollections.ErrNotFound)
 	}
