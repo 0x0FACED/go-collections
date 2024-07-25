98bc0467f46b603023d1719693150dd530b5b544
0x0FACED <https.genius@gmail.com>
2024-07-25 18:24:55 +0300

    upd: Insert test

--- trees/red_black_tree_test.go
+++ trees/red_black_tree_test.go
@@ -25,6 +25,12 @@ func TestRBT_Insert(t *testing.T) {
 	tr.Insert(6)
 	tr.Insert(3)
 	tr.Insert(15)
+	fmt.Println(tr.root.right.val)
+	tr.Insert(11)
 
 	fmt.Println(tr.InOrder())
+	fmt.Println(tr.root.val)
+	fmt.Println(tr.root.left.val)
+	fmt.Println(tr.root.right.val)
+	tr.PrintTree()
 }
