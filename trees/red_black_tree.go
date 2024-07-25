a52ca860618c30a739ad76d89b96027c7edd49c1
0x0FACED <https.genius@gmail.com>
2024-07-25 17:31:09 +0300

    added: Search impl,  1/3 of Delete method

--- trees/red_black_tree.go
+++ trees/red_black_tree.go
@@ -1,5 +1,11 @@
 package trees
 
+import (
+	"fmt"
+
+	gocollections "github.com/0x0FACED/go-collections"
+)
+
 // rbt - Red-Black Tree
 type rbt[T comparable] struct {
 	root *rbt_node[T]
@@ -18,11 +24,38 @@ func (rbt *rbt[T]) Insert(item T) {
 }
 
 func (rbt *rbt[T]) Delete(item T) error {
-	panic("not implemented") // TODO: Implement
+	node := rbt.searchHelper(rbt.root, item)
+	if node == nil {
+		return fmt.Errorf(gocollections.ErrNotFound)
+	}
+
+	if node.left == nil && node.right == nil {
+		if node == node.parent.left {
+			node.parent.left = nil
+		} else {
+			node.parent.right = nil
+		}
+		return nil
+	}
+
+	if (node.left != nil && node.right == nil) ||
+		(node.left == nil && node.right != nil) {
+
+		// TODO: delete when only 1 child
+		return nil
+	}
+
+	// TODO: delete when 2 children
+	return nil
 }
 
 func (rbt *rbt[T]) Search(item T) (*T, error) {
-	panic("not implemented") // TODO: Implement
+	node := rbt.searchHelper(rbt.root, item)
+	if node != nil {
+		return &node.val, nil
+	}
+	return nil, fmt.Errorf(gocollections.ErrNotFound)
+
 }
 
 func (rbt *rbt[T]) InOrder() []T {
