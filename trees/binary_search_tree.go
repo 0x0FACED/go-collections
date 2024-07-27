80bc0f31b2ceffe8cb8bb20bcfa552f95346af4d
0x0FACED <https.genius@gmail.com>
2024-07-27 14:15:46 +0300

    [bst] added: mutex using in methods

--- trees/binary_search_tree.go
+++ trees/binary_search_tree.go
@@ -1,8 +1,12 @@
 package trees
 
+import "sync"
+
 // bst - Binary Search Tree
 type bst[T comparable] struct {
 	root *node[T]
+
+	mu sync.Mutex
 
 	// comparator is the main func to compare two values of type T
 	//
@@ -15,37 +19,58 @@ func NewBST[T comparable](compare Comparator[T]) *bst[T] {
 }
 
 func (bst *bst[T]) Insert(item T) {
+	bst.mu.Lock()
+	defer bst.mu.Unlock()
+
 	bst.root = bst.insertHelper(bst.root, item)
 }
 
 func (bst *bst[T]) Delete(item T) error {
+	bst.mu.Lock()
+	defer bst.mu.Unlock()
+
 	var err error
 	bst.root, err = bst.deleteHelper(bst.root, item)
 	return err
 }
 
 func (bst *bst[T]) Search(item T) (*T, error) {
+	bst.mu.Lock()
+	defer bst.mu.Unlock()
+
 	return bst.searchHelper(bst.root, item)
 }
 
 func (bst *bst[T]) PreOrder() []T {
+	bst.mu.Lock()
+	defer bst.mu.Unlock()
+
 	var items []T
 	bst.preOrderHelper(bst.root, &items)
 	return items
 }
 
 func (bst *bst[T]) InOrder() []T {
+	bst.mu.Lock()
+	defer bst.mu.Unlock()
+
 	var items []T
 	bst.inOrderHelper(bst.root, &items)
 	return items
 }
 
 func (bst *bst[T]) PostOrder() []T {
+	bst.mu.Lock()
+	defer bst.mu.Unlock()
+
 	var items []T
 	bst.postOrderHelper(bst.root, &items)
 	return items
 }
 
 func (bst *bst[T]) LevelOrder() []T {
+	bst.mu.Lock()
+	defer bst.mu.Unlock()
+
 	return bst.levelOrderHelper()
 }
