ae2a47f09802ae17cbfd001d1b964b24dd55b3ab
0x0FACED <https.genius@gmail.com>
2024-07-29 14:23:45 +0300

    added: intfc for deque and priority queue

--- queue/queue.go
+++ queue/queue.go
@@ -27,3 +27,41 @@ type Queue[T comparable] interface {
 
 	IsFull() bool
 }
+
+type doubleEnded[T comparable] interface {
+	FrontEnqueue(item T) error
+	FrontDequeue() (*T, error)
+	FrontPeek() (*T, error)
+}
+
+type Deque[T comparable] interface {
+	Queue[T]
+	doubleEnded[T]
+}
+
+// pq is the interface for Priority Queue.
+//
+// Because pq methods are different from the others.
+//
+// Enqueue uses priority to insert the item and balance struct.
+//
+// DequeueMax() deletes and returns item with max priority.
+//
+// DequeueMin() with min priority.
+//
+// PeekMax() same as DequeueMax(), but doesn't remove.
+//
+// PeekMin() same as DequeueMin(), but doesn't remove.
+type pq[T comparable] interface {
+	Enqueue(item T, priority int) error
+
+	DequeueMax() (*T, error)
+	DequeueMin() (*T, error)
+
+	PeekMax() (*T, error)
+	PeekMin() (*T, error)
+
+	IsEmpty() bool
+
+	Size() int
+}
