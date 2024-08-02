7ab7427fb310020be9d6fa38c4cfadd97ed7a74d
0x0FACED <https.genius@gmail.com>
2024-08-02 14:42:49 +0300

    added: main for examples

---
+++ examples/heaps_ex/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import heapsex "github.com/0x0FACED/go-collections/examples/heaps_ex"
+
+func main() {
+	// to see this example: ./examples/heaps/min_heap.go
+	heapsex.Example_MinHeap()
+
+	// to see this example: ./examples/heaps/max_heap.go
+	heapsex.Example_MaxHeap()
+}
